f5da3d1727e35b50cc1dd93e925e9a03054ca81e
gabrielxpanda <gabrielxpanda@gmail.com>
2020-02-03 14:29:34 +0800

    fix

--- leetcode53.playground/Contents.swift
+++ leetcode53.playground/Contents.swift
@@ -2,53 +2,29 @@
 import UIKit
 
 class Solution {
-    
-    func maxSubArray(_ nums: [Int]) -> Int {
-
-        if nums.count < 1  { return 0 }
-        if nums.count == 1 { return nums[0]}
-
+    func lengthOfLastWord(_ s: String) -> Int {
         
-        var maxsum = 0
-        var temp = 0
-        var maxunderzero = 0
-        var underzerocount = 0
-        
-        for index in 0...nums.count-1 {
-            
-            if nums[index] < 0 {
-                underzerocount = underzerocount + 1
-                if maxunderzero == 0 {
-                  maxunderzero = nums[index]
-                }else {
-                    if nums[index] > maxunderzero {
-                        maxunderzero = nums[index]
-                    }
-                }
-                
-                if temp + nums[index] > 0 {
-                    temp = temp + nums[index]
-                }else{
-                    temp = 0
-                }
-                
-            }else{
-                temp = temp + nums[index]
-            }
-            
-            if temp >  maxsum{
-                maxsum = temp
-            }
+        if s.count == 0 {
+            return 0
         }
         
-        if underzerocount == nums.count {
-            return maxunderzero
+        if !s.contains(" ") {
+            return s.count
         }
-        return maxsum
-
+        
+        var len = 0
+        
+        for num in 0...s.count-1 {
+            let char:Character = s[s.index(s.startIndex,offsetBy: s.count-1-num)]
+            if char == Character.init(" ")  {
+                len = num
+                break
+            }
+        }
+        return len
+        
     }
 }
 
+print("s=\(Solution().lengthOfLastWord("Hello World"))")
 
-
- print("maxnum=\(Solution().maxSubArray([-2,-1]))")
